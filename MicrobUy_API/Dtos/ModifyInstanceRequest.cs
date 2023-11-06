99fb673beee253aa45ba318f412415c91575f190
GLOBANT\gaston.perazza <gaston.perazza@globant.com>
2023-11-06 18:44:31 -0300

    agregué dominio en modifyInstanceRequest dto

--- MicrobUy_API/Dtos/ModifyInstanceRequest.cs
+++ MicrobUy_API/Dtos/ModifyInstanceRequest.cs
@@ -6,6 +6,7 @@ namespace MicrobUy_API.Dtos
     {
         public string Nombre { get; set; }
         public string Logo { get; set; }
+        public string Dominio { get; set; }
         public string Tematica { get; set; }
         public string Description { get; set; }
         public EsquemaColoresEnum EsquemaColores { get; set; }
